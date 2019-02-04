0bd0145332b1e31bd7ba6b3dd793ca8dffc44a16
delightful-map-2010 <savyo.yohana@yahoo.com>
2019-02-04 20:16:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-let newID=1000000018
-let currentAge
+const newID = 1000000000 + oldID;
+
+const ageIsValid = Number.isInteger(currentAge);
+
+const randomNumber = Math.random() * 20;
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + 1000000001;
